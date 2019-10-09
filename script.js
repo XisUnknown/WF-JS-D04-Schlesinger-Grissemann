5d3aa7b5403a69d2f49a4811464081c36f03247a
Wojciech Schlesinger <wojciech.schlesinger@gmail.com>
2019-10-09 10:59:43 +0200

    changes

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-var mydata = JSON.parse(books);
 var title;
 var author;
 var read;
@@ -6,19 +5,23 @@ var p_title;
 var p_author;
 var p_read;
 
+
 function createBook(){
-    for(var i = 0;i<mydata.length;i++){
-        title=mydata[i].title;
-        author=mydata[i].author;
-        read = mydata[i].read;
+    for(var i = 0;i<books.length;i++){
+        console.log(books[0]);
+        title=books[i].title;
+        author=books[i].author;
+        read = books[i].read;
+        console.log(title);
         p_title = document.createElement("p");
         p_author = document.createElement("p");
         p_read = document.createElement("p");
         p_title.innerHTML = title;
         p_author.innerHTML = author;
         p_read.innerHTML = read;
-        document.getElementById("Book"+(i+1)).appendChild(p_title);
-        document.getElementById("Book"+(i+1)).appendChild(p_author);
-        document.getElementById("Book"+(i+1)).appendChild(p_read);
+        document.getElementById("book"+(i+1)).appendChild(p_title);
+        document.getElementById("book"+(i+1)).appendChild(p_author);
+        document.getElementById("book"+(i+1)).appendChild(p_read);
     }
 }
+createBook();
